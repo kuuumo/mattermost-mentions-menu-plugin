5138bac4c2cd793d2cb2041f60cadcce128f8cdd
hayato-marv <48147104+kuuumo@users.noreply.github.com>
2025-03-03 17:33:35 +0900

    Webアプリプラグインのエントリポイントを追加

---
+++ webapp/src/index.js
@@ -0,0 +1,18 @@
+import {Store} from 'redux';
+
+import manifest from '../manifest';
+import {LeftSidebar} from './components/left_sidebar';
+
+// eslint-disable-next-line import/no-unresolved
+import {PluginRegistry} from 'mattermost-webapp/plugins/registry';
+
+export default class Plugin {
+    /**
+     * @param {PluginRegistry} registry - Mattermost Plugin Registry
+     */
+    initialize(registry) {
+        registry.registerLeftSidebarHeaderComponent(LeftSidebar);
+    }
+}
+
+window.registerPlugin(manifest.id, new Plugin());
